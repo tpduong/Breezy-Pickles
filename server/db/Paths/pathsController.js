53bbfde48c754f696887ae5248335ac13d70aba9
Kim Ha <kimberlygha@gmail.com>
2015-11-11 20:13:02 -0800

    Added a path filter function and a helper parser function

--- server/db/Paths/pathsController.js
+++ server/db/Paths/pathsController.js
@@ -29,8 +29,46 @@ module.exports = {
   },
 
   filterPaths: function (req, res, next) {
-    var parsed = url.parse(req.url, true);
-    console.log(req.params.query);
+    var searchQuery = parser(req.params.query); // using parser helper function to create an object from our url path
+    Path.find(serachQuery)
+    .then(function (paths) {
+      if(paths){
+        res.send(paths);
+      } else {
+        res.status(400).send("No Paths that fit this search query");
+      }
+    }, function (err) {
+      console.error(err);
+      res.send(500);
+    });
   }
 
 };
+
+// helper parser function for our path --> turns into an objecct that becomes a search query
+var parser = function (string) {
+  var key = value = '';
+  var result = {};
+  var currentIsKey=true;
+  for(var i=0; i<string.length; i++){
+    if(string[i] === '=' || string[i] === '&'){
+      if(!currentIsKey){
+        result[key] = value;
+        key = value = '';
+      }
+      currentIsKey = !currentIsKey;
+    } else {
+      if(currentIsKey){
+        key += string[i];
+      }else{
+        value += string[i];
+      }
+    }
+  }
+  result[key] = value;
+  return result;
+};
+
+/* url path after search should come in the style of 
+key1=value1&key2=value2
+so it can be easily parsed */
