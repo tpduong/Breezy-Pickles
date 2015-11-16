c1ceec271b59dcb98e833742101adaa6a20b7897
CJ Apel <cjapel@CJs-MacBook-Air.local>
2015-11-16 10:40:16 -0800

    Start to populate dashboard with map divs

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,7 +1,17 @@
 angular.module('breezy.dashboard', [])
 
-.controller('DashboardController', function ($scope, $timeout, Maps, Users) {
+.controller('DashboardController', ['$scope', 'Users', '$http', function ($scope, Users, $http) {
   $scope.signout = function() {
     Users.signout();
   };
-});
+  $scope.paths = [];
+
+  $http.get('/paths').then(function(paths) {
+    $scope.paths = paths;
+    console.log('Successfully populated');
+  });
+  $scope.populate = function (index) {
+    console.log(index);
+    console.log(document.getElementById(index));
+  };
+}]);
