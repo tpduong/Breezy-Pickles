5b122e9057620c610b130da9376c933583c81edc
Tracy Duong <tpduong1@gmail.com>
2016-01-18 15:53:01 -0800

    working on dashboard

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -8,14 +8,17 @@ angular.module('breezy.dashboard', [])
     Users.signout();
   };
   $scope.maps = [];
-
+  $scope.searchTerm = "";
   $scope.polylines = {};
 
-  $http.get('/paths').then(function(maps) {
-    $scope.maps = maps.data;
-  });
+  var allMaps = [];
+  var userMaps = [];
+
+
 
   $scope.populate = function (index) {
+
+    console.log("index", index);
     if (!$scope.polylines[index]){    // avoid re-rendering maps
       $scope.polylines[index] = new google.maps.Polyline({
         strokeColor: '#000000',
@@ -29,7 +32,7 @@ angular.module('breezy.dashboard', [])
 
       var mapDiv = document.getElementById(index);
       var mapInfo = $scope.maps[index];
-      var map = new google.maps.Map(mapDiv, { 
+      var map = new google.maps.Map(mapDiv, {
         center: {lat: +mapInfo['center'][0], lng: +mapInfo['center'][1]},
         zoom: mapInfo['zoom'],
         mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -38,4 +41,45 @@ angular.module('breezy.dashboard', [])
       $scope.polylines[index].setMap(map);
     }
   };
+
+  // $scope.search = function() {
+
+  // };
+
+  // $scope.renderMaps = function() {
+  //   for (var i = 0; i < $scope.maps.length; i++) {
+  //     $scope.populate(0);
+
+  //   }
+  // };
+
+  $scope.displayMyMaps = function() {
+    console.log('pushed one');
+    $scope.maps.push(userMaps[0]);
+    // $scope.renderMaps();
+
+  };
+
+  $scope.displayAllMaps = function() {
+    console.log('all maps======')
+    console.dir(allMaps);
+    console.log('=========');
+    $scope.maps.push(allMaps[2]);
+    console.dir($scope.maps);
+  };
+
+
+  $http.get('/paths').then(function(maps) {
+    allMaps = maps.data.reverse();
+
+    for (var i = 0; i < allMaps.length; i++) {
+      if(allMaps[i].createdBy === localStorage.currentUser) {
+        userMaps.push(allMaps[i]);
+      }
+    }
+    $scope.displayMyMaps();
+
+  });
+
 }]);
+
